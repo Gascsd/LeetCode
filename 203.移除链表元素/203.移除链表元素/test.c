57d3b45a585a3854dccc8e6467f70166ec74d3bd
@gascsd <3311922913@qq.com>
2022-08-02 17:30:23 +0800

    ..

--- 203.移除链表元素/203.移除链表元素/test.c
+++ 203.移除链表元素/203.移除链表元素/test.c
@@ -1,31 +1,70 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 //https://leetcode.cn/problems/remove-linked-list-elements/
+ 
+
+// 解法一
+//struct ListNode* removeElements(struct ListNode* head, int val) {
+//    struct ListNode* prev = NULL;
+//    struct ListNode* cur = head;
+//    while (cur)
+//    {
+//        if (cur->val == val)
+//        {
+//            if (cur == head)
+//            {
+//                head = head->next;
+//                free(cur);
+//                cur = head;
+//            }
+//            else
+//            {
+//                prev->next = cur->next;
+//                free(cur);
+//                cur = prev->next;
+//            }
+//        }
+//        else
+//        {
+//            prev = cur;
+//            cur = cur->next;
+//        }
+//    }
+//    return head;
+//}
+
+
+
+//解法二
 struct ListNode* removeElements(struct ListNode* head, int val) {
-    struct ListNode* prev = NULL;
+    struct ListNode* newhead = NULL, * tail = NULL;
     struct ListNode* cur = head;
     while (cur)
     {
-        if (cur->val == val)
+        if (cur->val != val)
         {
-            if (cur == head)
+            //尾插
+            if (tail == NULL)//放入头节点
             {
-                head = head->next;
-                free(cur);
-                cur = head;
+                newhead = tail = cur;
             }
-            else
+            else//放入其他节点
             {
-                prev->next = cur->next;
-                free(cur);
-                cur = prev->next;
+                tail->next = cur;
+                tail = tail->next;
             }
+            cur = cur->next;
         }
         else
         {
-            prev = cur;
+            struct ListNode* del = cur;
             cur = cur->next;
+            free(del);
         }
     }
-    return head;
+    //防止最后一个节点存放val元素会有野指针的问题
+    if (tail)
+        tail->next = NULL;
+
+    return newhead;
 }
