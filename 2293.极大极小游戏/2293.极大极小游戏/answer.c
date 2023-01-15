3ac9f383d8aa69d23a920ab0f9fe48816466e7a7
@gascsd <3311922913@qq.com>
2023-01-15 19:39:20 +0800

    力扣每日一题 2293.极大极小游戏

---
+++ 2293.极大极小游戏/2293.极大极小游戏/answer.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+int Max(int a, int b)
+{
+    return a > b ? a : b;
+}
+int Min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+int minMaxGame(int* nums, int numsSize)
+{
+    if (numsSize == 1)
+        return nums[0];
+    else
+    {
+        numsSize /= 2;
+        int* newNums = (int*)malloc(sizeof(int) * numsSize);
+        assert(newNums);
+        for (int i = 0; i < numsSize; i += 2)
+        {
+            newNums[i] = Min(nums[2 * i], nums[2 * i + 1]);
+        }
+        for (int i = 1; i < numsSize; i += 2)
+        {
+            newNums[i] = Max(nums[2 * i], nums[2 * i + 1]);
+        }
+        return minMaxGame(newNums, numsSize);
+    }
+}
