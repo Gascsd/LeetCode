a12ea0c339f03860e1345dd34519b8d725ef63af
Gascsd <3311922913@qq.com>
2023-03-31 11:40:36 +0800

    杨辉三角解答

--- 118.杨辉三角/118.杨辉三角/answer.cpp
+++ 118.杨辉三角/118.杨辉三角/answer.cpp
@@ -24,12 +24,15 @@ using namespace std;
 //};
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> generate(int numRows) 
+    {
         vector<vector<int>> ret(numRows);
-        for (int i = 0; i < numRows; ++i) {
+        for (int i = 0; i < numRows; ++i) 
+        {
             ret[i].resize(i + 1);
             ret[i][0] = ret[i][i] = 1;
-            for (int j = 1; j < i; ++j) {
+            for (int j = 1; j < i; ++j) 
+            {
                 ret[i][j] = ret[i - 1][j] + ret[i - 1][j - 1];
             }
         }
